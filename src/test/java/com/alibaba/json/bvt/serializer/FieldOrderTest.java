dccf94d9dbf539463e40251fbe988b7c94fb96d3
IronMatt97 <matti1097@gmail.com>
2022-05-22 12:49:33 +0200

    removed tests and implemented parametrized ones

--- src/test/java/com/alibaba/json/bvt/serializer/FieldOrderTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/FieldOrderTest.java
@@ -2,48 +2,116 @@ package com.alibaba.json.bvt.serializer;
 
 import com.alibaba.fastjson.JSON;
 import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Created by wenshao on 2016/11/2.
  */
+@RunWith(Parameterized.class)
 public class FieldOrderTest extends TestCase {
-    public void test_field_order() throws Exception {
-        Person p = new Person();
+    Person person;
+    String result;
+    public FieldOrderTest(TestInput t) {
+        this.person = t.person;
+        this.result = t.result;
+    }
+    public static Collection<TestInput[]> configure() {
+        Collection<TestInput> inputs = new ArrayList<>();
+        Collection<TestInput[]> result = new ArrayList<>();
+        School validS = new School("Tor Vergata");
+        School emptyS = new School("");
+        School nullS = new School();
+        String validN = "Mario";
+        String emptyN = "";
+        String nullN = null;
+
+        //Istanza valida
+        Person validP = new Person(validN,validS);
+        //Combinazioni istanze con campi vuoti
+        Person emptyP1 = new Person(emptyN,validS);
+        Person emptyP2 = new Person(validN,emptyS);
+        Person emptyP3 = new Person(emptyN,emptyS);
+        //Combinazioni istanze nulle
+        Person nullP1 = new Person(nullN,validS);
+        Person nullP2 = new Person(validN,null);
+        Person nullP3 = new Person(nullN,null);
+        //Combinazioni istanze con il secondo parametro con il campo nullo
+        //Person nullP4 = new Person(nullN,validS);
+        Person nullP5 = new Person(validN,nullS);
+        Person nullP6 = new Person(nullN,nullS);
+        //Istanza nulla
+        Person nullP7 = null;
+
+        //Combinazioni dei vari test
+        inputs.add(new TestInput(validP,"{\"name\":\""+validN+"\",\"school\":{\"name\":\""+validS.getName()+"\"}}"));
+        inputs.add(new TestInput(emptyP1,"{\"name\":\"\",\"school\":{\"name\":\""+validS.getName()+"\"}}"));
+        inputs.add(new TestInput(emptyP2,"{\"name\":\""+validN+"\",\"school\":{\"name\":\"\"}}"));
+        inputs.add(new TestInput(emptyP3,"{\"name\":\"\",\"school\":{\"name\":\"\"}}"));
+        inputs.add(new TestInput(nullP1,"{\"school\":{\"name\":\""+validS.getName()+"\"}}"));
+        inputs.add(new TestInput(nullP2,"{\"name\":\""+validN+"\"}"));
+        inputs.add(new TestInput(nullP3,"{}"));
+        inputs.add(new TestInput(nullP5,"{\"name\":\""+validN+"\",\"school\":{}}"));
+        inputs.add(new TestInput(nullP6,"{\"school\":{}}"));
+        inputs.add(new TestInput(nullP7,"null"));
+
+        for (TestInput e : inputs) {
+            result.add(new TestInput[] { e });
+        }
+        return result;
+    }
+    @Parameters
+    public static Collection<TestInput[]> getTestParameters() {
+        return configure();
+    }
+    @Test
+    public void test_field_order() {
+        /*Person p = new Person();
         p.setName("njb");
         School s = new School();
         s.setName("llyz");
         p.setSchool(s);
         String json = JSON.toJSONString(p);
-        assertEquals("{\"name\":\"njb\",\"school\":{\"name\":\"llyz\"}}", json);
+        assertEquals("{\"name\":\"njb\",\"school\":{\"name\":\"llyz\"}}", json);*/
+
+        String json = JSON.toJSONString(this.person);
+        assertEquals(this.result, json);
     }
 
     public static class Person {
         private String name;
         private School school;
-
-        public boolean isSchool() {
-            return false;
+        public Person(){}
+        public Person(String name, School s){
+            this.name = name;
+            this.school = s;
         }
-
-        public School getSchool() {
-            return school;
-        }
-
         public void setSchool(School school) {
             this.school = school;
         }
-
-        public String getName() {
-            return name;
+        public School getSchool(){
+            return this.school;
         }
-
         public void setName(String name) {
             this.name = name;
         }
-
+        public String getName() {
+            return this.name;
+        }
     }
-
     public static class School {
+        public School(){}
+        public School(String s){
+            this.name = s;
+        }
         private String name;
 
         public String getName() {
@@ -52,6 +120,14 @@ public class FieldOrderTest extends TestCase {
 
         public void setName(String name) {
             this.name = name;
+        }
+    }
+    private static class TestInput {
+        Person person;
+        String result;
+        public TestInput(Person person, String result) {
+            this.person = person;
+            this.result = result;
         }
     }
 }
--- src/test/java/com/alibaba/json/bvt/serializer/ParserConfigTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/ParserConfigTest.java
@@ -1,27 +1,144 @@
 package com.alibaba.json.bvt.serializer;
 
-import org.junit.Assert;
+import java.lang.reflect.Type;
 
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
+import com.alibaba.fastjson.util.IdentityHashMap;
+import org.junit.Assert;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-import junit.framework.TestCase;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
 
-public class ParserConfigTest extends TestCase {
-
-    public void test_0() throws Exception {
-        ParserConfig config = new ParserConfig();
-        config.getDeserializers();
+@RunWith(Parameterized.class)
+public class ParserConfigTest {
+    String input;
+    Type clazz;
+    ParserConfig config;
+    Object expectedParam;
+    public ParserConfigTest(TestInput t) {
+        this.input = t.input;
+        this.clazz = t.clazz;
+        this.config = t.config;
+        this.expectedParam = t.expectedParam;
     }
-    
-    
-    public void test_1() throws Exception {
-        ParserConfig config = new ParserConfig(Thread.currentThread().getContextClassLoader());
-        
-        Model model = JSON.parseObject("{\"value\":123}", Model.class, config);
-        Assert.assertEquals(123, model.value);
+
+    public static Collection<TestInput[]> configure() {
+        Collection<TestInput> inputs = new ArrayList<>();
+        Collection<TestInput[]> result = new ArrayList<>();
+        ParserConfig config0 = new ParserConfig();
+        ParserConfig config1 = new ParserConfig(Thread.currentThread().getContextClassLoader());
+        //Combinazioni di stringa [valida-non valida-vuota-nulla], Type [valido-nullo] e ParserConfig [valido-unconfigured-nullo]
+        //Combinazioni con stringhe valide
+        inputs.add(new TestInput("{\"value\":123}",Model.class ,config0,123));
+        inputs.add(new TestInput("{\"value\":123}",Model.class ,config1,123));
+        inputs.add(new TestInput("{\"value\":123}",Model.class,null,null));
+        inputs.add(new TestInput("{\"value\":123}",null,config0,null));
+        inputs.add(new TestInput("{\"value\":123}",null,config1,null));
+        inputs.add(new TestInput("{\"value\":123}",null,null,null));
+
+        //Combinazioni con stringhe non valide
+        inputs.add(new TestInput("prova",Model.class,config0,null));
+        inputs.add(new TestInput("prova",Model.class,config1,null));
+        inputs.add(new TestInput("prova",Model.class,null,null));
+        inputs.add(new TestInput("prova",null,config0,null));
+        inputs.add(new TestInput("prova",null,config1,null));
+        inputs.add(new TestInput("prova",null,null,null));
+
+        //Combinazioni con stringhe vuote
+        inputs.add(new TestInput("",Model.class,config0,null));
+        inputs.add(new TestInput("",Model.class,config1,null));
+        inputs.add(new TestInput("",Model.class,null,null));
+        inputs.add(new TestInput("",null,config0,null));
+        inputs.add(new TestInput("",null,config1,null));
+        inputs.add(new TestInput("",null,null,null));
+
+        //Combinazioni con stringhe nulle
+        inputs.add(new TestInput(null,Model.class,config0,null));
+        inputs.add(new TestInput(null,Model.class,config1,null));
+        inputs.add(new TestInput(null,Model.class,null,null));
+        inputs.add(new TestInput(null,null,config0,null));
+        inputs.add(new TestInput(null,null,config1,null));
+        inputs.add(new TestInput(null,null,null,null));
+
+        for (TestInput e : inputs) {
+            result.add(new TestInput[] { e });
+        }
+        return result;
     }
-    
+    private static class TestInput {
+        String input;
+        Type clazz;
+        ParserConfig config;
+        Object expectedParam;
+        public TestInput(String input, Type clazz, ParserConfig config, Object e) {
+            this.input = input;
+            this.clazz = clazz;
+            this.config = config;
+            this.expectedParam = e;
+        }
+    }
+    @Parameters
+    public static Collection<TestInput[]> getTestParameters() {
+        return configure();
+    }
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Test
+    public void test_0() {
+        //ParserConfig config = new ParserConfig();
+        //config.getDeserializers();
+        if (Objects.isNull(this.config))
+            thrown.expect(NullPointerException.class);
+        IdentityHashMap<Type, ObjectDeserializer> deserializers = this.config.getDeserializers();
+        if (Objects.isNull(this.config))
+            Assert.assertNull(deserializers);
+        else
+            Assert.assertNotNull(deserializers);
+    }
+    @Test
+    public void test_1() {
+
+        //ParserConfig config = new ParserConfig(Thread.currentThread().getContextClassLoader());
+        //Model model = JSON.parseObject("{\"value\":123}", Model.class, 123);
+        //Assert.assertEquals(123, model.value);
+
+        boolean nullConfig =Objects.isNull(this.config);
+        boolean nullClazz =Objects.isNull(this.clazz);
+        boolean nullInput =Objects.isNull(this.input);
+        int nullParams = (nullClazz ?1:0) + (nullConfig?1:0) + (nullInput?1:0);
+        boolean invalidFormat=false;
+        if(!nullInput)
+            invalidFormat=!(this.input.startsWith("{\"value\":") && this.input.endsWith("}"));
+        //Nel caso in cui sia il formato che config che clazz sono null basta aspettarsi JSONException, whitebox è il primo che viene sollevato
+
+        if(!nullInput && !this.input.isEmpty() && nullConfig)
+            thrown.expect(NullPointerException.class);
+        else if (!nullInput && !this.input.isEmpty() && invalidFormat)
+            thrown.expect(JSONException.class);
+        else if(!nullInput && !this.input.isEmpty() && nullClazz)
+            thrown.expect(ClassCastException.class);
+        else if(nullInput){
+            if(nullParams <2)
+                thrown.expect(NullPointerException.class);
+        }
+        Model model = JSON.parseObject(this.input, this.clazz, this.config);
+
+        if(invalidFormat || nullClazz || nullConfig)
+            Assert.assertNull(model);
+        else
+            Assert.assertEquals(this.expectedParam, model.value);
+    }
     public static class Model {
         public int value;
     }
